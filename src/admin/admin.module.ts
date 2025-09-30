7b74a833fea7dc0b6cf154f14a98ced78cbeb8d2
tjens23 <tjens23@student.sdu.dk>
2025-09-30 22:14:28 +0200

    Enhance AdminModule by adding JWT authentication and dynamic configuration

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AdminSetupController } from './admin-setup.controller';
 import { AdminDashboardController } from './admin-dashboard.controller';
 import { RolesModule } from '../roles/roles.module';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [RolesModule, UsersModule],
+  imports: [
+    RolesModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: configService.get<string>('jwt.expiresIn') },
+      }),
+      inject: [ConfigService],
+    }),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [AdminSetupController, AdminDashboardController],
 })
 export class AdminModule {}
