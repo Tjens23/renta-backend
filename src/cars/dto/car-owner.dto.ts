fcf42a870e4cc5fc5955f4a3474831db56205bc2
tjens23 <tjens23@student.sdu.dk>
2025-10-02 15:13:48 +0200

    Refactor import statements in car-owner.dto.ts for improved readability

--- src/cars/dto/car-owner.dto.ts
+++ src/cars/dto/car-owner.dto.ts
@@ -1,4 +1,11 @@
-import { IsString, IsNumber, IsOptional, Min, Max, IsEmail } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  IsOptional,
+  Min,
+  Max,
+  IsEmail,
+} from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateCarOwnerDto {
