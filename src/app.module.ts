153cb8238c4f7c0ff9871b29872e1cffd67ee44c
tjens23 <tjens23@student.sdu.dk>
2025-10-04 11:19:33 +0200

    Refactor database connection settings in app.module.ts to use environment variables for improved configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,11 +31,11 @@ import { CarAvailable } from './entities/CarAvaliable';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Hyg57aff',
-      database: 'renta',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'renta',
       entities: [
         User,
         Role,
